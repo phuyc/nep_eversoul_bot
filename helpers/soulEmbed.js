ef2e29d886f5c5b28b64cea3ae5afad4befb8303
phuyc <lhphuc70@gmail.com>
2023-03-06 14:57:49 +0700

    Add pve_mid ratings

--- helpers/soulEmbed.js
+++ helpers/soulEmbed.js
@@ -29,7 +29,7 @@ async function soulEmbed(name) {
         .setFooter({ text: 'nepnep#1358', iconURL: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/BE/nl/19/EP0031-CUSA03124_00-AV00000000000037/image?w=320&h=320&bg_color=000000&opacity=100&_version=00_09_000' })
         .addFields(
             // Field 3 (Ratings)               
-            { name: 'RATINGS', value: `**PVE (Early/Late)**: ${RATINGS[json.ratings.pve_early] ?? '?'}/${RATINGS[json.ratings.pvp_late] ?? '?'}` },
+            { name: 'RATINGS', value: `**PVE (Early/Mid/Late)**: ${RATINGS[json.ratings.pve_early] ?? '?'}/${RATINGS[json.ratings.pve_mid] ?? '?'}/${RATINGS[json.ratings.pve_late] ?? '?'}` },
         );
 
     skills = json.skills;
@@ -47,8 +47,10 @@ async function soulEmbed(name) {
         for (description of descriptions) {
             if (description.marks[0]) {
                 if (description.marks[0].type === 'bold') fields += '**';
-            } 
+            }
+
             fields += description.value;
+
             if (description.marks[0]) {
                 if (description.marks[0].type === 'bold') fields += '**';
             }
